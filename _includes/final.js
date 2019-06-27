65498f8207e166724a5d9f7fff2b3d6883b4b64e
evanwill <evanpeterw@gmail.com>
2019-06-27 14:50:09 -0700

    update analytics to current umbrella

--- _includes/final.js
+++ _includes/final.js
@@ -1,27 +1,18 @@
-  <script> 
- 
-				
-  $(document).ready(function() {$("#phonepop").popover(); 
-    $("#textpop").popover();  });
-	</script>
-	
-
-	
-	
-	<!-- Google Analytics --> 
-<!-- Google Analytics – Library Umbrella --> 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-76328753-1', 'auto', {'allowLinker': true});
-  ga('require', 'linker');
-  ga('linker:autoLink', ['digital.lib.uidaho.edu', 'www.lib.uidaho.edu', 'vivo.nkn.uidaho.edu','poetry.lib.uidaho.edu','journals.lib.uidaho.edu','search.lib.uidaho.edu','inside.uidaho.edu'] ); 
-  ga('send', 'pageview');
-
+<script> 
+$(document).ready(function() {$("#phonepop").popover(); 
+  $("#textpop").popover();  });
 </script>
-
-<!-- End Google Analytics --> 
-
+ <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-76328753-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  /* load umbrella property with enhanced link attribution and cross domain tracking */
+  gtag('config', 'UA-76328753-1', {
+    'link_attribution': true,
+    'linker': {
+      'domains': ['www.lib.uidaho.edu', 'www.ijc.uidaho.edu', 'vivo.nkn.uidaho.edu', 'inside.uidaho.edu', 'www.insideidaho.org', 'libguides.uidaho.edu', 'digital.lib.uidaho.edu', 'poetry.lib.uidaho.edu', 'journals.lib.uidaho.edu']
+    }
+  });
+</script>
